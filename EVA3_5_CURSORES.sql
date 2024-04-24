4c439573d3b22e45f8f33530a98b303795602d63
SebastianR49 <118313737+SebastianR49@users.noreply.github.com>
2024-04-24 18:57:37 -0500

    Add files via upload

---
+++ EVA3_5_CURSORES.sql
@@ -0,0 +1,21 @@
+delimiter ||
+create procedure contar_actores_chafa(out conteo int)
+begin
+	declare terminar int default false;
+    declare val int;
+	declare cons_actores cursor for select actor_id from actor;
+    declare continue handler for not found
+	begin
+		set terminar = true;
+    end;
+    open cons_actores;
+    set conteo = -1;
+    contar_a: loop
+		fetch cons_actores into val;
+        set conteo = conteo + 1;
+        if terminar then
+			leave contar_a;
+		end if;
+    end loop;
+end||
+delimiter ;
